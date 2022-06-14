e2d9ee69dcd44e7c6bb8c73d97278e9f9e2c0651
Vegadrone <spenaantonino@gmail.com>
2022-06-14 15:29:24 +0200

    Verbalize the problem

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,6 @@
-console.log ('ciao')
+//chiedo all'utente kmNumber e passengerAge attraverso il prompt (in parseInt)
+    //il prezzo del biglietto ticketPrice è definitio da kmNumber * kmPrice (0,21 centesimi di euro)
+    //sconto del 20% ((ticketPrice * 20) / 100 = discount ) if passengerAge è < di 18
+    //sconto del 40% ((ticketPrice * 40) / 100 = discount ) if passengerAge è > di 65
+// A questo punto sottraggo da ticketPrice il discount (ticketPrice - discount) e ottengo
+//discountedTicketPrice
